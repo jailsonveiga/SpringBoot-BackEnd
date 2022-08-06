ef679934118bb69a22543274ecedd827b3546358
jailsonveiga <jmdaveiga2012@gmail.com>
2022-08-06 14:25:57 -0400

    Implements

--- src/main/java/jay/springboot/backend/serices/impl/EmployeeServicesImpl.java
+++ src/main/java/jay/springboot/backend/serices/impl/EmployeeServicesImpl.java
@@ -1,4 +1,22 @@
 package jay.springboot.backend.serices.impl;
 
-public class EmployeeServicesImpl {
+import jay.springboot.backend.model.Employee;
+import jay.springboot.backend.repository.EmployeeRepository;
+import jay.springboot.backend.service.EmployeeService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmployeeServicesImpl implements EmployeeService {
+
+    private EmployeeRepository employeeRepository;
+
+    public EmployeeServicesImpl(EmployeeRepository employeeRepository) {
+        super();
+        this.employeeRepository = employeeRepository;
+    }
+
+    @Override
+    public Employee saveEmployee(Employee employee) {
+        return employeeRepository.save(employee);
+    }
 }
