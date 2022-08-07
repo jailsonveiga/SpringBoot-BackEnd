4712c2372713c770cbadd05ebaab3385c859bf82
jailsonveiga <jmdaveiga2012@gmail.com>
2022-08-07 17:58:52 -0400

    Get all the employees REST API

--- src/main/java/jay/springboot/backend/controller/EmployeeController.java
+++ src/main/java/jay/springboot/backend/controller/EmployeeController.java
@@ -4,10 +4,9 @@ import jay.springboot.backend.model.Employee;
 import jay.springboot.backend.service.EmployeeService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/employees")
@@ -24,5 +23,11 @@ public class EmployeeController {
     public ResponseEntity<Employee> saveEmployee(@RequestBody Employee employee) {
 
         return new ResponseEntity<Employee>(employeeService.saveEmployee(employee), HttpStatus.CREATED);
+    }
+
+    // build get all employees REST API
+    @GetMapping()
+    public List<Employee> getAllEmployees() {
+        return employeeService.getAllEmployees();
     }
 }
