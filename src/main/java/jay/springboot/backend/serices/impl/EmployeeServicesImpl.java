40f58738457543b191a7b33ea50dbda32ee127b9
jailsonveiga <jmdaveiga2012@gmail.com>
2022-08-07 18:28:07 -0400

    Implements get employee by ID

--- src/main/java/jay/springboot/backend/serices/impl/EmployeeServicesImpl.java
+++ src/main/java/jay/springboot/backend/serices/impl/EmployeeServicesImpl.java
@@ -1,11 +1,13 @@
 package jay.springboot.backend.serices.impl;
 
+import jay.springboot.backend.exception.ResourceNotFoundException;
 import jay.springboot.backend.model.Employee;
 import jay.springboot.backend.repository.EmployeeRepository;
 import jay.springboot.backend.service.EmployeeService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class EmployeeServicesImpl implements EmployeeService {
@@ -25,5 +27,16 @@ public class EmployeeServicesImpl implements EmployeeService {
     @Override
     public List<Employee> getAllEmployees() {
         return employeeRepository.findAll();
+    }
+
+    @Override
+    public Employee getEmployeeById(long id) {
+        Optional<Employee> employee = employeeRepository.findById(id);
+
+        if(employee.isPresent()) {
+            return employee.get();
+        }else {
+            throw new ResourceNotFoundException("Employee", "Id", id);
+        }
     }
 }
