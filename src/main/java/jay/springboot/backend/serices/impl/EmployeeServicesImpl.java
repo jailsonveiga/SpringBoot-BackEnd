0ece5dd1e02dac9e70a540ebc58bc3482b43a804
jailsonveiga <jmdaveiga2012@gmail.com>
2022-08-07 19:08:04 -0400

    Implements Update Employee

--- src/main/java/jay/springboot/backend/serices/impl/EmployeeServicesImpl.java
+++ src/main/java/jay/springboot/backend/serices/impl/EmployeeServicesImpl.java
@@ -39,4 +39,21 @@ public class EmployeeServicesImpl implements EmployeeService {
             throw new ResourceNotFoundException("Employee", "Id", id);
         }
     }
+
+    @Override
+    public Employee updateEmployee(Employee employee, long id) {
+
+        // we need to check whether employee with given id exist in DB or not
+        Employee existingEmployee = employeeRepository.findById(id).orElseThrow(
+                () -> new ResourceNotFoundException("Employee", "Id", id));
+
+        existingEmployee.setFirstName(employee.getFirstName());
+        existingEmployee.setLastName(employee.getLastName());
+        existingEmployee.setEmail(employee.getEmail());
+
+        // save existing employee to DB
+        employeeRepository.save(existingEmployee);
+
+        return existingEmployee;
+    }
 }
