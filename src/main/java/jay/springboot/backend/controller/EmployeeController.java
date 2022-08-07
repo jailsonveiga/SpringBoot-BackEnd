b52f426ed613ba6881c0bd47af2a2f0f56ce7c40
jailsonveiga <jmdaveiga2012@gmail.com>
2022-08-07 17:43:58 -0400

    Create Employee Rest API

--- src/main/java/jay/springboot/backend/controller/EmployeeController.java
+++ src/main/java/jay/springboot/backend/controller/EmployeeController.java
@@ -1,4 +1,28 @@
 package jay.springboot.backend.controller;
 
+import jay.springboot.backend.model.Employee;
+import jay.springboot.backend.service.EmployeeService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/employees")
 public class EmployeeController {
+
+    private EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    // build create employee REST API
+    @PostMapping()
+    public ResponseEntity<Employee> saveEmployee(@RequestBody Employee employee) {
+
+        return new ResponseEntity<Employee>(employeeService.saveEmployee(employee), HttpStatus.CREATED);
+    }
 }
