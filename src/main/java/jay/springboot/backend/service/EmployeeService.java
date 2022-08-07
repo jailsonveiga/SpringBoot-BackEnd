ec8e0c2c3dde0cb5e9fa9c21309bbc8825fdc7f5
jailsonveiga <jmdaveiga2012@gmail.com>
2022-08-07 17:52:34 -0400

    Interface get List of employees

--- src/main/java/jay/springboot/backend/service/EmployeeService.java
+++ src/main/java/jay/springboot/backend/service/EmployeeService.java
@@ -2,6 +2,9 @@ package jay.springboot.backend.service;
 
 import jay.springboot.backend.model.Employee;
 
+import java.util.List;
+
 public interface EmployeeService {
     Employee saveEmployee(Employee employee);
+    List<Employee> getAllEmployees();
 }
